f00e3c25e1a01d3f6bd2dd3a5fa21cf3d454d447
Sebastian Gutierrez <45526748+SebastianIsmaelG@users.noreply.github.com>
2025-02-20 03:36:24 -0400

    Update usp_busqueda_atenciones_completadas.sql

--- procedures/usp_busqueda_atenciones_completadas.sql
+++ procedures/usp_busqueda_atenciones_completadas.sql
@@ -1,17 +1,12 @@
 DELIMITER $$
---
--- Verificar si la base de datos existe antes de continuar
---
-IF NOT EXISTS (SELECT COUNT(*) FROM information_schema.schemata WHERE schema_name = 'vollmed' THEN 
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error: La base de datos vollmed no existe.';
-END IF $$
 
-USE vollmed $$
---
--- ----------------------------------
---
 CREATE PROCEDURE `busqueda_atenciones_completadas`()  
 BEGIN
+    -- Verificar si la base de datos existe antes de continuar
+    IF NOT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'vollmed') THEN 
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error: La base de datos vollmed no existe.';
+    END IF;
+
     -- Cuenta cuántos pacientes fueron atendidos en el año actual
     SELECT CONCAT("Hemos tratado ", COUNT(*), " pacientes con éxito durante el año ", YEAR(CURDATE())) 
            AS resultado
